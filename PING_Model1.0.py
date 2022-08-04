edd20361390f484583c2d03beb6d62a11b23a019
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-08-03 21:56:40 -0400

    ex - run mult sims to explore param space

--- PING_Model1.0.py
+++ PING_Model1.0.py
@@ -176,203 +176,209 @@ def create_n_BallAndStick(n):
     return cells
 
 
-#Setup connection matrix for a 4x4 and number of cells
-connection_matrix = np.ones((num_cells,num_cells))
-# E -> E = 1
-# E -> I = 1
-# I -> E = 0.5 of excitatories
-# All self connections = 0
-
-my_cells = create_n_BallAndStick(num_cells)
-# h.topology()
-
-for i in range (0,num_cells):
-    connection_matrix[i][i] = 0         #Self connections
-    
-
-#for each inhibitory neuron, removes connections with the first half of excitatory neurons
-for i, cell in enumerate(my_cells):
-    if cell._cell_type == "Inhibitory":
-        inhibitory_con = []
-        for j, value in enumerate(connection_matrix[i]):
-            if value == 1 and my_cells[j]._cell_type == "Excitatory":
-                inhibitory_con.append(j)
-            if value == 1 and my_cells[j]._cell_type == "Inhibitory":
-                connection_matrix[i][j] = 0
-        for index in random.sample(inhibitory_con, len(inhibitory_con)//2):
-            connection_matrix[i][index] = 0
-                
+# here add for loops
+import numpy
+for exc_mean in numpy.arange(0.05,0.35,0.1):
+    for inh_mean in numpy.arange(0.05,0.35,0.1):
+        
+        #Setup connection matrix for a 4x4 and number of cells
+        connection_matrix = np.ones((num_cells,num_cells))
+        # E -> E = 1
+        # E -> I = 1
+        # I -> E = 0.5 of excitatories
+        # All self connections = 0
+        
+        my_cells = create_n_BallAndStick(num_cells)
+        # h.topology()
+        
+        for i in range (0,num_cells):
+            connection_matrix[i][i] = 0         #Self connections
             
-
-# print(connection_matrix)
-conmat = []
-for row in connection_matrix:
-    r = []
-    for col in row:
-        r.append('x ' if col > 0 else ' ')
-    conmat.append(r)
-
-print("Cells:")       
-print (*my_cells, sep="\n")
-print("\nConnections:")
-header = [c._cell_type[:3] + str(c._gid) for c in my_cells]
-import pandas as pd
-df = pd.DataFrame(conmat, 
-                  columns = header, 
-                  index=header)
-print('Pre |            Post')
-print(df)
-
-#Set up stimulations
-
-# Set an iclamp to each cell
-#Does this Clamp go throughout the simulation?
-#for cell in my_cells:
-    #stimClamp = h.IClamp(cell.soma(0.5))
-    #stimClamp.delay = 5
-    #stimClamp.dur = 100
-    #stimClamp.amp = 0.11
-'''
-    else:
-        stimClamp = h.IClamp(cell.soma(0.5))
-        stimClamp.delay = 5
-        stimClamp.dur = 100
-        stimClamp.amp = 0.3
-'''
-     # if cell._cell_type == "Excitatory":
-    #Amp between 0.034 and 0.165 for 10 cells
-    #Amp seems to vary depending on number of neurons in network
-# set a synaptic input onto my_cells[0]
-# from an artificial stim cell
-syn_ = h.ExpSyn(my_cells[0].dend(0.5))
-
-stim = h.NetStim()
-stim.number = 1     #Assigned number of stimulation
-stim.start = 9      #start of stimulus in ms
-ncstim = h.NetCon(stim, syn_)   #Looks at potential of stim, and once that hits threshold, triggers syn_
-ncstim.delay = 1 * ms           #1 ms delay to model time for NTs or signal propogation
-ncstim.weight[0] = 0.04       #how strong the transmission is from pre to post synapse
-
-syn_.tau = 2 * ms
-
-
-
-
-#Connecting the cells
-
-
-con_loc = []
-for count_row, row in enumerate(connection_matrix):
-    for count_column, value in enumerate(row):
-        if value == 1:
-            con_loc.append([count_row, count_column])
-'''
-Basically goes through the each row and 
-'''
+        
+        #for each inhibitory neuron, removes connections with the first half of excitatory neurons
+        for i, cell in enumerate(my_cells):
+            if cell._cell_type == "Inhibitory":
+                inhibitory_con = []
+                for j, value in enumerate(connection_matrix[i]):
+                    if value == 1 and my_cells[j]._cell_type == "Excitatory":
+                        inhibitory_con.append(j)
+                    if value == 1 and my_cells[j]._cell_type == "Inhibitory":
+                        connection_matrix[i][j] = 0
+                for index in random.sample(inhibitory_con, len(inhibitory_con)//2):
+                    connection_matrix[i][index] = 0
+                        
+                    
+        
+        # print(connection_matrix)
+        conmat = []
+        for row in connection_matrix:
+            r = []
+            for col in row:
+                r.append('x ' if col > 0 else ' ')
+            conmat.append(r)
+        
+        print("Cells:")       
+        print (*my_cells, sep="\n")
+        print("\nConnections:")
+        header = [c._cell_type[:3] + str(c._gid) for c in my_cells]
+        import pandas as pd
+        df = pd.DataFrame(conmat, 
+                          columns = header, 
+                          index=header)
+        print('Pre |            Post')
+        print(df)
+        
+        #Set up stimulations
+        
+        # Set an iclamp to each cell
+        #Does this Clamp go throughout the simulation?
+        #for cell in my_cells:
+            #stimClamp = h.IClamp(cell.soma(0.5))
+            #stimClamp.delay = 5
+            #stimClamp.dur = 100
+            #stimClamp.amp = 0.11
+        '''
+            else:
+                stimClamp = h.IClamp(cell.soma(0.5))
+                stimClamp.delay = 5
+                stimClamp.dur = 100
+                stimClamp.amp = 0.3
+        '''
+             # if cell._cell_type == "Excitatory":
+            #Amp between 0.034 and 0.165 for 10 cells
+            #Amp seems to vary depending on number of neurons in network
+        # set a synaptic input onto my_cells[0]
+        # from an artificial stim cell
+        syn_ = h.ExpSyn(my_cells[0].dend(0.5))
+        
+        stim = h.NetStim()
+        stim.number = 1     #Assigned number of stimulation
+        stim.start = 9      #start of stimulus in ms
+        ncstim = h.NetCon(stim, syn_)   #Looks at potential of stim, and once that hits threshold, triggers syn_
+        ncstim.delay = 1 * ms           #1 ms delay to model time for NTs or signal propogation
+        ncstim.weight[0] = 0.04       #how strong the transmission is from pre to post synapse
+        
+        syn_.tau = 2 * ms
         
         
-syns = []
-netcons = []
-for row in con_loc:
-    source_index, target_index = row
-    source = my_cells[source_index]
-    target = my_cells[target_index]
-   # syn = h.ExpSyn(target.dend(0.5))   #syn is the synapse and later appended to list syns
-    if source._cell_type == "Excitatory":
-        nc = h.NetCon(source.soma(0.5)._ref_v, target.esyn, sec=source.soma)    #Still need help understanding netcons
-        nc.weight[0] = 0.1    #strength of connection between pre and post cells (maximum conductance)
-        nc.delay = 5            #ms - delay between presynaptic cell reaching threshold and postsynaptic cell triggering (time to propogate down axon and NT)
-                                #if planning to try to shorten frequency of gamma waves in ping model, probably mess around with tau (decay factor of synapse) rather than delay
-    else: # Inhibitory
-        nc = h.NetCon(source.soma(0.5)._ref_v, target.isyn, sec=source.soma)    #Still need help understanding netcons
-        nc.weight[0] = 0.25    #strength of connection between pre and post cells (maximum conductance)
-        nc.delay = 5            #ms - delay between presynaptic cell reaching threshold and postsynaptic cell triggering (time to propogate down axon and NT)
-                                #if planning to try to shorten frequency of gamma waves in ping model, probably mess around with tau (decay factor of synapse) rather than delay
-
-    netcons.append(nc)
-  #  syns.append(syn)
-
-
-
-
-'''
-colors = ['g', 'b', 'r', 'y']    
-fig, ax = plt.subplots()    #Sets up figure with legend and certain number of subplots (in this case 0)
-plt.xlabel('t (ms)')        #Add labels to plot
-plt.ylabel('v (mV)')
-for cell, colour in zip(my_cells, colors):
-    recording_cell = cell
-    soma_v = h.Vector().record(recording_cell.soma(0.5)._ref_v)
-    dend_v = h.Vector().record(recording_cell.dend(0.5)._ref_v)
-    t = h.Vector().record(h._ref_t)
-    h.finitialize(-65 * mV)
-    h.continuerun(100 * ms)
-    ax.plot(t, soma_v, f'-{colour}', label='soma(0.5)' if recording_cell == my_cells[0] else None)
-    ax.plot(t, dend_v, f'--{colour}', label='dend(0.5)' if recording_cell == my_cells[0] else None)
-
-leg = ax.legend();
-'''
-
-
-
-#Recording values
-for cell in my_cells:
-    #recording_cell = my_cells[0]
-    cell.vrec_soma = h.Vector().record(cell.soma(0.5)._ref_v)
-    cell.vrec_dend = h.Vector().record(cell.dend(0.5)._ref_v)
-    
-t = h.Vector().record(h._ref_t)
-     
-LightSource = Optrode(my_cells[0].soma) # provide a section that the
-                                        # light source should be near
-
-#%% Run simulation 
-h.finitialize(-65 * mV)
-h.continuerun(simdur * ms)
-
-
-#%% Plotting
-fig = plt.figure(figsize= (7,11))
-for i, cell in enumerate(my_cells):
-    ax = fig.add_subplot(len(my_cells),1,i+1)
-    plt.plot(t, cell.vrec_soma, label='soma(0.5)')
-    plt.plot(t, cell.vrec_dend, label='dend(0.5)')
-    plt.legend()
-    plt.title(cell._cell_type + ' ' + str(cell._gid) + (" + channel rhodopsin" if cell._express_cr else ""))
-    if i<(len(my_cells) - 1):
-        plt.tick_params(axis='x', labelbottom=False) 
-
-#%%
-#Showing neuron firing and times
-
-fig = plt.figure() 
-inh = 0
-exc = 0
-for cell in my_cells:
-    if cell._cell_type == "Excitatory":
-        exc += 1
-    else:
-        inh += 1
-    # print(cell._gid if cell._cell_type == "Excitatory" else -cell._gid)
-    gidvec = [cell._gid if cell._cell_type == "Excitatory" else -1-cell._gid]*len(cell.spike_times)  
-    plt.plot(cell.spike_times, gidvec, marker = 'o', linestyle="None", color = 'b' if cell._cell_type == "Excitatory" else 'r')    
-
-plt.ylabel("Cell #")
-plt.title("Spike Raster")
-plt.xlabel("Time (ms)")
-plt.yticks(range(-inh,exc))
-plt.show()
-
-
-listosoma = [x.soma for x in my_cells]
-import riseplot as r
-r.plot(listosoma)
-
-print("Finished")
-
-
-
-
-
-
-
+        
+        
+        #Connecting the cells
+        
+        
+        con_loc = []
+        for count_row, row in enumerate(connection_matrix):
+            for count_column, value in enumerate(row):
+                if value == 1:
+                    con_loc.append([count_row, count_column])
+        '''
+        Basically goes through the each row and 
+        '''
+                
+                
+        syns = []
+        netcons = []
+        for row in con_loc:
+            source_index, target_index = row
+            source = my_cells[source_index]
+            target = my_cells[target_index]
+           # syn = h.ExpSyn(target.dend(0.5))   #syn is the synapse and later appended to list syns
+            if source._cell_type == "Excitatory":
+                nc = h.NetCon(source.soma(0.5)._ref_v, target.esyn, sec=source.soma)    #Still need help understanding netcons
+                nc.weight[0] = 0.1    #strength of connection between pre and post cells (maximum conductance)
+                nc.delay = 5            #ms - delay between presynaptic cell reaching threshold and postsynaptic cell triggering (time to propogate down axon and NT)
+                                        #if planning to try to shorten frequency of gamma waves in ping model, probably mess around with tau (decay factor of synapse) rather than delay
+            else: # Inhibitory
+                nc = h.NetCon(source.soma(0.5)._ref_v, target.isyn, sec=source.soma)    #Still need help understanding netcons
+                nc.weight[0] = 0.25    #strength of connection between pre and post cells (maximum conductance)
+                nc.delay = 5            #ms - delay between presynaptic cell reaching threshold and postsynaptic cell triggering (time to propogate down axon and NT)
+                                        #if planning to try to shorten frequency of gamma waves in ping model, probably mess around with tau (decay factor of synapse) rather than delay
+        
+            netcons.append(nc)
+          #  syns.append(syn)
+        
+        
+        
+        
+        '''
+        colors = ['g', 'b', 'r', 'y']    
+        fig, ax = plt.subplots()    #Sets up figure with legend and certain number of subplots (in this case 0)
+        plt.xlabel('t (ms)')        #Add labels to plot
+        plt.ylabel('v (mV)')
+        for cell, colour in zip(my_cells, colors):
+            recording_cell = cell
+            soma_v = h.Vector().record(recording_cell.soma(0.5)._ref_v)
+            dend_v = h.Vector().record(recording_cell.dend(0.5)._ref_v)
+            t = h.Vector().record(h._ref_t)
+            h.finitialize(-65 * mV)
+            h.continuerun(100 * ms)
+            ax.plot(t, soma_v, f'-{colour}', label='soma(0.5)' if recording_cell == my_cells[0] else None)
+            ax.plot(t, dend_v, f'--{colour}', label='dend(0.5)' if recording_cell == my_cells[0] else None)
+        
+        leg = ax.legend();
+        '''
+        
+        
+        
+        #Recording values
+        for cell in my_cells:
+            #recording_cell = my_cells[0]
+            cell.vrec_soma = h.Vector().record(cell.soma(0.5)._ref_v)
+            cell.vrec_dend = h.Vector().record(cell.dend(0.5)._ref_v)
+            
+        t = h.Vector().record(h._ref_t)
+             
+        LightSource = Optrode(my_cells[0].soma) # provide a section that the
+                                                # light source should be near
+        
+        #%% Run simulation 
+        h.finitialize(-65 * mV)
+        h.continuerun(simdur * ms)
+        
+        
+        #%% Plotting
+        fig = plt.figure(figsize= (7,11))
+        for i, cell in enumerate(my_cells):
+            ax = fig.add_subplot(len(my_cells),1,i+1)
+            plt.plot(t, cell.vrec_soma, label='soma(0.5)')
+            plt.plot(t, cell.vrec_dend, label='dend(0.5)')
+            plt.legend()
+            plt.title(cell._cell_type + ' ' + str(cell._gid) + (" + channel rhodopsin" if cell._express_cr else ""))
+            if i<(len(my_cells) - 1):
+                plt.tick_params(axis='x', labelbottom=False) 
+        
+        #%%
+        #Showing neuron firing and times
+        
+        fig = plt.figure() 
+        inh = 0
+        exc = 0
+        for cell in my_cells:
+            if cell._cell_type == "Excitatory":
+                exc += 1
+            else:
+                inh += 1
+            # print(cell._gid if cell._cell_type == "Excitatory" else -cell._gid)
+            gidvec = [cell._gid if cell._cell_type == "Excitatory" else -1-cell._gid]*len(cell.spike_times)  
+            plt.plot(cell.spike_times, gidvec, marker = 'o', linestyle="None", color = 'b' if cell._cell_type == "Excitatory" else 'r')    
+        
+        plt.ylabel("Cell #")
+        plt.title("Spike Raster")
+        plt.xlabel("Time (ms)")
+        plt.yticks(range(-inh,exc))
+        plt.show()
+        
+        #"spikeraster" + str(exc_mean) + str(inh_mean) + ".dat"
+        # print(exc_mean, inh_mean, latest_spike_network)        
+        listosoma = [x.soma for x in my_cells]
+        import riseplot as r
+        r.plot(listosoma)
+        
+        print("Finished")
+        
+        
+        
+        
+        
+        
+        
