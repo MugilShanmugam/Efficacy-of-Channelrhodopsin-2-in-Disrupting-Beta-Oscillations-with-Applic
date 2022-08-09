ce7d824f9f1b9feed5d439123c1906f7f844f796
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-08-08 20:45:08 -0400

    low beta

--- Ping_Model_Consolidated.py
+++ Ping_Model_Consolidated.py
@@ -8,6 +8,22 @@ Created on Sat Jul 30 16:50:22 2022
 from neuron import h, gui
 from neuron.units import ms, mV
 h.load_file('stdrun.hoc')
+runtime = 1000
+thresh = -10
+h.dt = 0.025
+nsegval = 5
+distribution = 0.5 # expression level of channel rhodopsin
+exprtypes = '' # or 'ei' or 'i' or '',
+               # type of cell to express in
+# optogenetics parameters
+h('absorbance_coefficient = 0.1249') # (1/mm) # Range: (0.05233, 0.1975)
+h('scatter_coefficient = 7.37')      # (1/mm) # Range: (6.679, 8.062)
+ 
+h('forall delete_section()')
+
+from OptoLight import OptoLight
+from Optrode import Optrode
+
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -19,10 +35,15 @@ class Cell:         #Creation of a generic class called cell
     def __init__(self, gid, x, y, z, cell_type):  
         self._cell_type = cell_type
         self._gid = gid
+        self._express_cr = False
         if self._cell_type == 'e':
             self._cell_type = "Excitatory"
+            if 'e' in exprtypes:
+                self._express_cr = True
         else:
             self._cell_type = 'Inhibitory'
+            if 'i' in exprtypes:
+                self._express_cr = True
         self._name = 'BallAndStick, {}'.format(self._cell_type)
         self._setup_morphology()
         self.all = self.soma.wholetree()    
@@ -38,6 +59,7 @@ class Cell:         #Creation of a generic class called cell
         self.vrec_dend = None
         self._spike_detector = h.NetCon(self.soma(0.5)._ref_v, None, sec=self.soma)
         self.spike_times = h.Vector()
+        self._spike_detector.threshold = thresh
         self._spike_detector.record(self.spike_times)
         
     def __repr__(self):
@@ -45,6 +67,14 @@ class Cell:         #Creation of a generic class called cell
             #Formatted so it will return name[gid(aka the neuron number)]
     def _set_position(self, x, y, z):
         self.x, self.y, self.z = x, y, z
+        
+    def seg_section_distance(self,seg,root_section=None):
+        """ Returns the distance between each segment of section, and the
+        root_section
+        """
+        if not root_section:root_section=self.soma
+        h.distance(0, root_section(0.5).x, sec=root_section)
+        return h.distance(seg.x, sec=seg.sec)
             
 class BallAndStick(Cell):
     #Creation of subclass under cell so BallAndStick will have methods defined under both Cell and BallAndStick
@@ -53,12 +83,13 @@ class BallAndStick(Cell):
         if cell_type == "e":
             self.stimClampE = h.IClamp(self.soma(0.5))
             self.stimClampE.delay = 5
-            self.stimClampE.dur = 400
-            self.stimClampE.amp = 0.032
+            self.stimClampE.dur = runtime
+            self.stimClampE.amp = 0.35 # 0.25 # 0.032
+
         else:
             self.stimClampI = h.IClamp(self.soma(0.5))
             self.stimClampI.delay = 5
-            self.stimClampI.dur = 400
+            self.stimClampI.dur = runtime
             self.stimClampI.amp = 0.032
     def _setup_morphology(self):
         self.soma = h.Section(name='soma', cell=self)
@@ -68,7 +99,9 @@ class BallAndStick(Cell):
         self.dend.L = 200
         self.dend.diam = 1
     def _setup_biophysics(self, cell_type):
+        self.fiberD=2.0
         for sec in self.all:
+            sec.nseg = nsegval
             sec.Ra = 100    # Axial resistance in Ohm * cm
             sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
         self.soma.insert('hh')  
@@ -98,6 +131,23 @@ class BallAndStick(Cell):
         self.isyn.tau1 = 0.2 * ms
         self.isyn.tau2 = 10 * ms
         self.isyn.e = -80 * mV
+        
+                # add chanrhod mechanism to cells that express it:
+        max_distance = 0
+        if self._express_cr:
+            # assuming only one dendrite section
+            # if more, would need to iterate through
+            # whole dendrite list and insert into each
+            self.dend.insert("chanrhod")
+            self.lightmech = OptoLight(self)
+            for seg in self.dend:
+                max_distance = max([max_distance, self.seg_section_distance(seg)])
+                distance=self.seg_section_distance(seg)
+                if max_distance != 0: # check that not on soma
+                    s = (max_distance-distance)/(max_distance) # soma centric weighting
+                    a = (distance)/(max_distance)              # apical centric weighting
+                    W = distribution
+                    seg.channel_density_chanrhod  =  s*(1-W) + a*W
             
 def create_n_BallAndStick(n):
     """n = number of cells; r = radius of circle"""
@@ -133,15 +183,6 @@ for i, cell in enumerate(my_cells):
         for index in random.sample(inhibitory_con, len(inhibitory_con)//2):
             connection_matrix[i][index] = 0
 
-syn_ = h.ExpSyn(my_cells[0].dend(0.5))
-stim = h.NetStim()
-stim.number = 1     #Assigned number of stimulation
-stim.start = 9      #start of stimulus in ms
-ncstim = h.NetCon(stim, syn_)   #Looks at potential of stim, and once that hits threshold, triggers syn_
-ncstim.delay =  1 * ms           #1 ms delay to model time for NTs or signal propogation
-ncstim.weight[0] = 0.04    #how strong the transmittions is from pre to post synapse
-syn_.tau = 2 * ms
-
 con_loc = []
 for count_row, row in enumerate(connection_matrix):
     for count_column, value in enumerate(row):
@@ -157,16 +198,24 @@ for row in con_loc:
     target = my_cells[target_index]
     distance = math.sqrt((source.x - target.x)**2 + (source.y - target.y)**2)
    # syn = h.ExpSyn(target.dend(0.5))   #syn is the synapse and later appended to list syns
-    if source._cell_type == "Excitatory":
+    if source._cell_type == "Excitatory" and target._cell_type == "Excitatory":
         nc = h.NetCon(source.soma(0.5)._ref_v, target.esyn, sec=source.soma)    
-        nc.weight[0] = 0.1  
-        nc.delay = random.gauss(abs((distance)/100)+6,1)
+        nc.weight[0] = random.gauss(0.008,0.002) 
+        nc.delay = random.gauss(abs((distance)/1000)+3,0.05) 
+        nc.threshold = thresh
+        delays_list.append(nc.delay)
+    elif source._cell_type == "Excitatory": # onto inhibitory
+        nc = h.NetCon(source.soma(0.5)._ref_v, target.esyn, sec=source.soma)    
+        nc.weight[0] = 0.15                                                                                                                                                                                                                                     #random.gauss(0.05,0.01) 
+        nc.delay = random.gauss(abs((distance)/1000)+3,0.05) 
+        nc.threshold = thresh
         delays_list.append(nc.delay)
     else: # Inhibitory
         nc = h.NetCon(source.soma(0.5)._ref_v, target.isyn, sec=source.soma) 
-        nc.weight[0] = 0.8  #strength of connection between pre and post cells (maximum conductance)
-        nc.delay = random.gauss(abs((distance)/100)+6,1)  
+        nc.weight[0] = 0.2 #random.gauss(0.20,0.07)  #strength of connection between pre and post cells (maximum conductance)
+        nc.delay = random.gauss(abs((distance)/1000)+3,0.05)  
         delays_list.append(nc.delay)
+        nc.threshold = thresh
     netcons.append(nc)
     
 #Recording values
@@ -175,14 +224,19 @@ for cell in my_cells:
     cell.vrec_dend = h.Vector().record(cell.dend(0.5)._ref_v) 
 t = h.Vector().record(h._ref_t)
         
-runtime = 400
-soma_Vms = np.zeros(16001)
-dend_Vms = np.zeros(16001)
+soma_Vms = np.zeros(int(runtime/h.dt)+1)
+dend_Vms = np.zeros(int(runtime/h.dt)+1)
 #%% 
-# Plotting
+LightSource = Optrode(my_cells[0].soma) # provide a section that the
+                                        # light source should be near
 
+
+#%%
+# Plotting
+h.tstop = runtime
+h.stdinit()
 h.finitialize(-65 * mV)
-h.continuerun(400 * ms)
+h.run()
 #fig = plt.figure(figsize= (7,11))
 for i, cell in enumerate(my_cells):
     #ax = fig.add_subplot(len(my_cells),1,i+1)
@@ -199,8 +253,10 @@ for i, cell in enumerate(my_cells):
 soma_Vms = soma_Vms/100
 dend_Vms = dend_Vms/100
 fig2 = plt.figure()
-plt.plot(t, soma_Vms)
-plt.plot(t, dend_Vms)
+plt.plot(t, soma_Vms, label="soma")
+plt.plot(t, dend_Vms, label="dend")
+plt.legend()
+plt.title("LFP")
 plt.show()
 #%%
 # FFT time
@@ -220,13 +276,13 @@ plt.plot(soma_freq, np.abs(X), 'b')
 plt.title("Soma FFT")
 plt.xlabel('Freq (Hz)')
 plt.ylabel('FFT Amplitude |X(freq)|')
-plt.xlim(5, 100)
-plt.ylim(0,20000)
+plt.xlim(int((999/runtime)*2), 100)
+#plt.ylim(0,20000)
 
 
 sr = 1000/h.dt
-X = fft(dend_Vms - np.mean(dend_Vms))
-N = len(X)
+dendX = fft(dend_Vms - np.mean(dend_Vms))
+N = len(dendX)
 n = np.arange(N)
 T = N/sr
 dend_freq = n/T 
@@ -234,22 +290,22 @@ dend_freq = n/T
 plt.figure(figsize = (12, 6))
 plt.subplot(121)
 
-plt.plot(dend_freq, np.abs(X), 'b')
+plt.plot(dend_freq, np.abs(dendX), 'b')
 plt.title("Dendrite FFT")
 plt.xlabel('Freq (Hz)')
 plt.ylabel('FFT Amplitude |X(freq)|')
-plt.xlim(5, 100)
-plt.ylim(0,20000)
+plt.xlim((999/runtime)*2, 100)
+#plt.ylim(0,20000)
 #%%
 # Implementing bandpower
-
+fft_results = np.abs(dendX) # MB updated 
 beta_range = []
-for frequency in soma_freq:
+for i,frequency in enumerate(dend_freq): # MB updated 
     if frequency >= 19 and frequency <= 21:
-        beta_range.append(frequency)
-
-bandpower = sum(fft(beta_range)) / len(beta_range)
-print(bandpower)
+        beta_range.append(fft_results[i]) # MB updated 
+        
+bandpower = sum(beta_range)/len(beta_range) # MB updated 
+print(f"Dendritic beta bandpower: {bandpower:.2f}")
 #%%
 # Showing neuron firing and times
 
@@ -263,7 +319,8 @@ for cell in my_cells:
         inh += 1
     #print(cell._gid if cell._cell_type == "Excitatory" else -cell._gid)
     gidvec = [cell._gid if cell._cell_type == "Excitatory" else -1-cell._gid]*len(cell.spike_times)  
-    plt.plot(cell.spike_times, gidvec, marker = 'o', linestyle="None", color = 'b' if cell._cell_type == "Excitatory" else 'r')    
+    if len(gidvec) > 0:
+        plt.plot(cell.spike_times, gidvec, marker = 'o', linestyle="None", color = 'b' if cell._cell_type == "Excitatory" else 'r')    
 
 plt.ylabel("Cell #")
 plt.title("Spike Raster")
